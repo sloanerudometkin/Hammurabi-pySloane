1f72a5a55210d13dc86f6c5aacd2fb281e68c94f
sloane <sloanerudometkin@gmail.com>
2026-07-19 14:22:09 -0400

    plague deaths

--- hammurabi.py
+++ hammurabi.py
@@ -21,4 +21,12 @@ class Hammurabi:
         yield_per_acre = 3
         rats_eaten_last_year = 200
 
+     # CALCULATION METHODS AS OUTLINED IN THE TEST
+
+    def plagueDeaths(self, population):
+        # if there's a 15% chance of plague, half the population dies
+        if self.rand.randint(0, 99) < 15:
+            return population // 2
+        return 0
+
        
