9ce8d06db17ccc8d97921d60634a00baa1176ed8
sloane <sloanerudometkin@gmail.com>
2026-07-19 13:29:55 -0400

    started skelton

---
+++ hammurabi.py
@@ -0,0 +1,18 @@
+import random
+
+class Hammurabi:
+    def __init__(self):
+        self.rand = random.Random()
+
+    def main(self):
+        self.playGame()
+
+    def playGame(self):
+        # declare local variables here: grain, population, etc.
+        # statements go after the declarations
+
+    # other methods go here
+
+if __name__ == "__main__":
+    hammurabi = Hammurabi()
+    hammurabi.main()
