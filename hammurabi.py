8ab30bbe7a146da6c8beab2e5684c53a70efb4ba
sloane <sloanerudometkin@gmail.com>
2026-07-19 14:37:54 -0400

    starting over

--- hammurabi.py
+++ hammurabi.py
@@ -8,25 +8,12 @@ class Hammurabi:
         self.playGame()
 
     def playGame(self):
-        # INITIAL STATE 
-        population = 100
-        bushels = 2800
-        acres_owned = 1000
-        land_value = 19
-        
-        # to track statistics for summaries
-        starved_last_year = 0
-        immigrants_last_year = 5
-        harvested_last_year = 3000
-        yield_per_acre = 3
-        rats_eaten_last_year = 200
+        # declare local variables here: grain, population, etc.
+        # statements go after the declarations
 
-     # CALCULATION METHODS AS OUTLINED IN THE TEST
+    # other methods go here
 
-    def plagueDeaths(self, population):
-        # if there's a 15% chance of plague, half the population dies
-        if self.rand.randint(0, 99) < 15:
-            return population // 2
-        return 0
-
+if __name__ == "__main__":
+    hammurabi = Hammurabi()
+    hammurabi.main()
        
