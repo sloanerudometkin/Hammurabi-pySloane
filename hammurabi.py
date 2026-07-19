3e8391035aedab6cb451fa6a06ebc1b7c660c59a
sloane <sloanerudometkin@gmail.com>
2026-07-19 15:39:49 -0400

    ask how many acres to sell function and imlemented in core game loop

--- hammurabi.py
+++ hammurabi.py
@@ -27,7 +27,7 @@ class Hammurabi:
             print(" Welcome to Year {year})")
             self.printSummary(year, starved_last_year, immigrants_last_year, population, harvested_last_year, yield_per_acre, rats_eaten_last_year, acres_owned, land_value, bushels)
 
-            # call 1st input method
+            # call 1st input method : buy land
             acres_to_buy = self.askHowManyAcresToBuy(land_value, bushels)
             if acres_to_buy == -1:
                 print("\nYou have abandoned your kingdom! Shame on you! It will fall into chaos...")
@@ -35,6 +35,14 @@ class Hammurabi:
             #then update game state variables
             bushels -= acres_to_buy * land_value
             acres_owned += acres_to_buy
+
+            if acres_to_buy == 0:
+                acres_to_sell = self.askHowManyAcresToSell(acres_owned)
+                if acres_to_sell == -1:
+                    print("\nYou have abonded your kingdom! Shame on you! It will fall into chaos...")
+
+                bushels += acres_to_sell * land_value
+                acres_owned -= acres_to_sell
 
     #print summary
     def printSummary(self, year, starved, immigrants, population, harvest, yield_per_acre, rats_eaten, acres_owned, land_value, bushels):
@@ -65,6 +73,21 @@ class Hammurabi:
             except ValueError:
                 print("Please enter a valid whole number.")
 
+    def askHowManyAcresToSell(self, acresOwned):
+        while True:
+            try:
+                user_input = input("O Great Hammurabi, how many acres of land do you want to sell? (or type 'quit' to exit): ").strip().lower()
+                if user_input == 'quit':
+                    return -1
+                acres = int(user_input)
+                if acres < 0:
+                    print("Well... you can't sell a negative amount of land!")
+                elif acres > acresOwned:
+                    print(f"O Great Hammurabi, you are such a jest! We only own {acresOwned} acres!")
+                else:
+                    return acres
+            except ValueError:
+                print("please enter a valid whole number or 'quit'.")
     
 
     # other methods go here
